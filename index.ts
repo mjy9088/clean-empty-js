2652725ef620c8ba8b0062f81d6240595633076f
Juyeong Maing <mjy9088@naver.com>
2024-08-29 21:14:10 +0900

    add ability to accept multiple glob patterns

--- index.ts
+++ index.ts
@@ -1,29 +1,40 @@
 import { glob } from "glob";
 import { readFile, unlink } from "node:fs/promises";
 
-const defaultGlobPattern = "**/*.js";
 const contentToRemove = `"use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 `;
 
-async function removeEmptyJsFiles(pattern: string = defaultGlobPattern) {
-  try {
-    const files = await glob(pattern);
-    for (const file of files) {
-      const content = await readFile(file, "utf-8");
-      if (content === contentToRemove) {
-        await unlink(file);
-        console.log(`Removed: ${file}`);
-      }
-    }
-  } catch (error: any) {
-    console.error(`Error: ${error.message}`);
-  }
-}
-
 async function main() {
-  const pattern = process.argv[2] || defaultGlobPattern;
-  await removeEmptyJsFiles(pattern);
+  const [_, __, ...patterns] = process.argv;
+  if (patterns.length === 0) {
+    console.error("Please provide a directory path.");
+    process.exit(1);
+  }
+
+  let status = 0;
+
+  for (const pattern of patterns) {
+    await removeEmptyJsFiles(pattern);
+  }
+
+  process.exit(status);
+
+  async function removeEmptyJsFiles(pattern: string) {
+    try {
+      const files = await glob(pattern);
+      for (const file of files) {
+        const content = await readFile(file, "utf-8");
+        if (content === contentToRemove) {
+          await unlink(file);
+          console.log(`Removed: ${file}`);
+        }
+      }
+    } catch (error: any) {
+      console.error(`Error: ${error.message}`);
+      status = 1;
+    }
+  }
 }
 
 main().catch((error) => console.error(error));
